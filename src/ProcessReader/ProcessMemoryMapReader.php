86a62d5319203b017594e28565d6414e64d6aa87
sji <shinji.igarashi@gmail.com>
2020-03-16 07:06:50 +0900

    add /proc/<pid>/maps reader

---
+++ src/ProcessReader/ProcessMemoryMapReader.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the sj-i/php-profiler package.
+ *
+ * (c) sji <sji@sj-i.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace PhpProfiler\ProcessReader;
+
+/**
+ * Class ProcessMemoryMapReader
+ * @package PhpProfiler\ProcessReader
+ */
+class ProcessMemoryMapReader
+{
+    /**
+     * @param int $process_id
+     * @return string
+     */
+    public function read(int $process_id): string
+    {
+        return file_get_contents("/proc/{$process_id}/maps");
+    }
+}
---
+++ tests/ProcessReader/ProcessMemoryMapReaderTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the sj-i/php-profiler package.
+ *
+ * (c) sji <sji@sj-i.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PhpProfiler\ProcessReader;
+
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ProcessMemoryMapReaderTest
+ * @package PhpProfiler\ProcessReader
+ */
+class ProcessMemoryMapReaderTest extends TestCase
+{
+    public function testRead()
+    {
+        $result = (new ProcessMemoryMapReader())->read(getmypid());
+        $first_line = strtok($result, "\n");
+        $this->assertRegExp('/[0-9a-f]{12,16}-[0-9a-f]{12,16} [r\-][w\-][x\-][p\-] [0-9a-f]{8} [0-9][0-9]:[0-9][0-9] [0-9]+ +[^ ]+/', $first_line);
+    }
+}
