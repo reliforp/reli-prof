7c3eb11c7796a03db08bd93af4584cf09cc720c0
sji <shinji.igarashi@gmail.com>
2020-04-11 07:45:24 +0900

    add a class to read symbols from outer php process

---
+++ src/ProcessReader/PhpSymbolReaderCreator.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * This file is part of the sj-i/php-profiler package.
+ *
+ * (c) sji <sji@sj-i.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace PhpProfiler\ProcessReader;
+
+
+use PhpProfiler\Lib\Elf\SymbolResolverCreator;
+use PhpProfiler\Lib\Elf\Tls\TlsFinder;
+use PhpProfiler\Lib\Process\MemoryReader;
+use PhpProfiler\Lib\Process\RegisterReader;
+
+/**
+ * Class PhpSymbolReaderCreator
+ * @package PhpProfiler\ProcessReader
+ */
+class PhpSymbolReaderCreator
+{
+    /**
+     * @var MemoryReader
+     */
+    private MemoryReader $memory_reader;
+
+    /**
+     * PhpSymbolReaderCreator constructor.
+     * @param MemoryReader $memory_reader
+     */
+    public function __construct(MemoryReader $memory_reader)
+    {
+        $this->memory_reader = $memory_reader;
+    }
+
+    /**
+     * @param int $pid
+     * @return ProcessModuleSymbolReader
+     * @throws \PhpProfiler\Lib\Process\MemoryReaderException
+     */
+    public function create(int $pid): ProcessModuleSymbolReader
+    {
+        $memory_reader = $this->memory_reader;
+
+        $symbol_reader_creator = new ProcessSymbolReaderCreator(
+            $pid,
+            ProcessMemoryMapCreator::create()->getProcessMemoryMap($pid),
+            new SymbolResolverCreator(),
+            $memory_reader
+        );
+
+        $tls_block_address = null;
+        $libpthread_symbol_reader = $symbol_reader_creator->createModuleReaderByNameRegex('/.*\/libpthread-2\.29\.so$/');
+        if (!is_null($libpthread_symbol_reader)) {
+            $tls_finder = new TlsFinder(
+                $symbol_reader_creator->createModuleReaderByNameRegex('/.*\/libpthread-2\.29\.so$/'),
+                new RegisterReader(),
+                $memory_reader
+            );
+            $tls_block_address = $tls_finder->findTlsBlock($pid, 1);
+        }
+
+        return $symbol_reader_creator->createModuleReaderByNameRegex('/.*\/php$/', $tls_block_address);
+    }
+}
