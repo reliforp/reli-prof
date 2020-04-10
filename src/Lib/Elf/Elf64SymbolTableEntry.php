d5f315155438d58966f2736cf3545d4516aa7f2a
sji <shinji.igarashi@gmail.com>
2020-04-11 07:15:37 +0900

    add a getter to test type of symbol table entry

--- src/Lib/Elf/Elf64SymbolTableEntry.php
+++ src/Lib/Elf/Elf64SymbolTableEntry.php
@@ -21,10 +21,53 @@ use PhpProfiler\Lib\UInt64;
  */
 class Elf64SymbolTableEntry
 {
+    public const STT_NOTYPE = 0;
+    public const STT_OBJECT = 1;
+    public const STT_FUNC = 2;
+    public const STT_SECTION = 3;
+    public const STT_FILE = 4;
+    public const STT_COMMON = 5;
+    public const STT_TLS = 6;
+    public const STT_LOOS = 10;
+    public const STT_HIOS = 12;
+    public const STT_LOPROC = 13;
+    public const STT_HIPROC = 15;
+
     public int $st_name; // Elf64_Word
     public int $st_info; // unsigned char
     public int $st_other; // unsigned char
     public int $st_shndx; // Elf64_Half
     public UInt64 $st_value; // Elf64_Addr
     public UInt64 $st_size; // Elf64_Xword
+
+    /**
+     * @return int
+     */
+    public function getType(): int
+    {
+        return (($this->st_info) & 0xf);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isTls(): bool
+    {
+        return $this->getType() === self::STT_TLS;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isUndefined(): bool
+    {
+        return $this->st_name === 0
+            and $this->st_info === 0
+            and $this->st_other === 0
+            and $this->st_shndx === 0
+            and $this->st_value->lo === 0
+            and $this->st_value->hi === 0
+            and $this->st_size->lo === 0
+            and $this->st_size->hi === 0;
+    }
 }
