0bf6b3c717b8fd59bb3c874110b592b8f4c70fb6
sji <shinji.igarashi@gmail.com>
2020-04-27 06:41:09 +0900

    add test case

--- tests/Lib/Elf/Tls/LibThreadDbTlsFinderTest.php
+++ tests/Lib/Elf/Tls/LibThreadDbTlsFinderTest.php
@@ -11,6 +11,7 @@
 
 namespace PhpProfiler\Lib\Elf\Tls;
 
+use FFI\CData;
 use Mockery;
 use PhpProfiler\Lib\Elf\Process\ProcessSymbolReaderInterface;
 use PhpProfiler\Lib\Process\MemoryReader\MemoryReaderInterface;
@@ -57,5 +58,54 @@ class LibThreadDbTlsFinderTest extends TestCase
             0x30000,
             $finder->findTlsBlock(1, 1)
         );
+    }
+
+    /**
+     * @dataProvider casesDebugSymbolsFoundOrNot
+     * @param CData|null $_thread_db_pthread_dtvp
+     * @param CData|null $_thread_db_dtv_dtv
+     * @param CData|null $_thread_db_dtv_t_pointer_val
+     * @throws TlsFinderException
+     */
+    public function testThrowTlsFinderExceptionIfDebugSymbolsNotFound(
+        ?CData $_thread_db_pthread_dtvp,
+        ?CData $_thread_db_dtv_dtv,
+        ?CData $_thread_db_dtv_t_pointer_val
+    ) {
+        $this->expectException(TlsFinderException::class);
+
+        $symbol_reader = Mockery::mock(ProcessSymbolReaderInterface::class);
+        $symbol_reader->expects()->read('_thread_db_pthread_dtvp')->andReturns($_thread_db_pthread_dtvp);
+        $symbol_reader->expects()->read('_thread_db_dtv_dtv')->andReturns($_thread_db_dtv_dtv);
+        $symbol_reader->expects()->read('_thread_db_dtv_t_pointer_val')->andReturns($_thread_db_dtv_t_pointer_val);
+
+        $thread_pointer_retriever = Mockery::mock(ThreadPointerRetrieverInterface::class);
+        $thread_pointer_retriever->expects()->getThreadPointer(1)->andReturns(0x10000);
+
+        $memory_reader = Mockery::mock(MemoryReaderInterface::class);
+
+        $finder = new LibThreadDbTlsFinder($symbol_reader, $thread_pointer_retriever, $memory_reader);
+        $finder->findTlsBlock(1, 1);
+    }
+
+    /**
+     * @return array<string, array<?CData>>
+     */
+    public function casesDebugSymbolsFoundOrNot(): array
+    {
+        $_thread_db_pthread_dtvp = \FFI::new('unsigned char[12]');
+        $_thread_db_dtv_dtv = \FFI::new('unsigned char[12]');
+        $_thread_db_dtv_t_pointer_val = \FFI::new('unsigned char[12]');
+        \FFI::memset($_thread_db_pthread_dtvp, 0, 12);
+        \FFI::memset($_thread_db_dtv_dtv, 0, 12);
+        \FFI::memset($_thread_db_dtv_t_pointer_val, 0, 12);
+        $_thread_db_pthread_dtvp[8] = 8;
+        $_thread_db_dtv_dtv[0] = 64;
+
+        return [
+            'pthread_dtvp not found' => [null, $_thread_db_dtv_dtv, $_thread_db_dtv_t_pointer_val],
+            'dtv_dtv not found' => [$_thread_db_pthread_dtvp, null, $_thread_db_dtv_t_pointer_val],
+            'dtv_t_pointer_val not found' => [$_thread_db_pthread_dtvp, $_thread_db_dtv_dtv, null],
+        ];
     }
 }
