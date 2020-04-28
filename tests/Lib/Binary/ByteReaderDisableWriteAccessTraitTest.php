69e0da3a314e04d00f2c220f08fd679eb895d761
sji <shinji.igarashi@gmail.com>
2020-04-29 06:28:13 +0900

    add test for ByteReaderDisableWriteAccessTrait

---
+++ tests/Lib/Binary/ByteReaderDisableWriteAccessTraitTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This file is part of the sj-i/php-profiler package.
+ *
+ * (c) sji <sji@sj-i.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PhpProfiler\Lib\Binary;
+
+use ArrayAccess;
+use LogicException;
+use PHPUnit\Framework\TestCase;
+
+class ByteReaderDisableWriteAccessTraitTest extends TestCase
+{
+
+    public function testOffsetSet()
+    {
+        $this->expectException(LogicException::class);
+        $instance = new class implements ArrayAccess {
+            use ByteReaderDisableWriteAccessTrait;
+
+            public function offsetGet($offset)
+            {
+                return 0xDEADBEAF;
+            }
+
+            public function offsetExists($offset)
+            {
+                false;
+            }
+        };
+        $instance[0] = 1;
+    }
+
+    public function testOffsetUnset()
+    {
+        $this->expectException(LogicException::class);
+        $instance = new class implements ArrayAccess {
+            use ByteReaderDisableWriteAccessTrait;
+
+            public function offsetGet($offset)
+            {
+                return 0xDEADBEAF;
+            }
+
+            public function offsetExists($offset)
+            {
+                false;
+            }
+        };
+        unset($instance[0]);
+    }
+}
