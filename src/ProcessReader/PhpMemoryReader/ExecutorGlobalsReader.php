687ecbfd304ba46cf590ee668d3b0cc8064c8e22
sji <shinji.igarashi@gmail.com>
2020-05-05 01:52:27 +0900

    separate function name reader to methods

--- src/ProcessReader/PhpMemoryReader/ExecutorGlobalsReader.php
+++ src/ProcessReader/PhpMemoryReader/ExecutorGlobalsReader.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace PhpProfiler\ProcessReader\PhpMemoryReader;
 
+use FFI\CData;
 use PhpProfiler\Lib\PhpInternals\ZendTypeReader;
 use PhpProfiler\Lib\Process\MemoryReader\MemoryReaderInterface;
 use PhpProfiler\Lib\Process\MemoryReader\MemoryReaderException;
@@ -37,6 +38,7 @@ final class ExecutorGlobalsReader
         $this->zend_type_reader = $zend_type_reader;
     }
 
+
     /**
      * @param int $pid
      * @param int $executor_globals_address
@@ -45,16 +47,45 @@ final class ExecutorGlobalsReader
      */
     public function readCurrentFunctionName(int $pid, int $executor_globals_address): string
     {
+        /** @var \FFI\PhpInternals\zend_executor_globals $eg */
+        $eg = $this->readExecutorGlobals($pid, $executor_globals_address);
+
+        /** @var \FFI\PhpInternals\zend_execute_data $current_execute_data */
+        $current_execute_data = $this->readCurrentExecuteData($pid, $eg);
+
+        /** @var \FFI\PhpInternals\zend_function $current_function */
+        $current_function = $this->readCurrentFunction($pid, $current_execute_data);
+
+        return $this->readFunctionName($pid, $current_function);
+    }
+
+    /**
+     * @param int $pid
+     * @param int $executor_globals_address
+     * @return CData
+     * @throws MemoryReaderException
+     */
+    public function readExecutorGlobals(int $pid, int $executor_globals_address): CData
+    {
         $eg_raw = $this->memory_reader->read(
             $pid,
             $executor_globals_address,
             $this->zend_type_reader->sizeOf('zend_executor_globals')
         );
-        /** @var \FFI\PhpInternals\zend_executor_globals $eg */
-        $eg = $this->zend_type_reader->readAs('zend_executor_globals', $eg_raw);
+        return $this->zend_type_reader->readAs('zend_executor_globals', $eg_raw);
+    }
 
+    /**
+     * @param int $pid
+     * @param CData $eg
+     * @return CData
+     * @throws MemoryReaderException
+     */
+    public function readCurrentExecuteData(int $pid, CData $eg): CData
+    {
         /**
          * @var \FFI\CPointer $current_execute_data_addr
+         * @var \FFI\PhpInternals\zend_executor_globals $eg
          * @psalm-suppress PropertyTypeCoercion
          */
         $current_execute_data_addr = \FFI::cast('long', $eg->current_execute_data);
@@ -63,11 +94,20 @@ final class ExecutorGlobalsReader
             $current_execute_data_addr->cdata,
             $this->zend_type_reader->sizeOf('zend_execute_data')
         );
-        /** @var \FFI\PhpInternals\zend_execute_data $current_execute_data */
-        $current_execute_data = $this->zend_type_reader->readAs('zend_execute_data', $current_execute_data_raw);
+        return $this->zend_type_reader->readAs('zend_execute_data', $current_execute_data_raw);
+    }
 
+    /**
+     * @param int $pid
+     * @param CData $current_execute_data
+     * @return CData
+     * @throws MemoryReaderException
+     */
+    public function readCurrentFunction(int $pid, CData $current_execute_data): CData
+    {
         /**
          * @var \FFI\CPointer $func_pointer
+         * @var \FFI\PhpInternals\zend_execute_data $current_execute_data
          * @psalm-suppress PropertyTypeCoercion
          */
         $func_pointer = \FFI::cast('long', $current_execute_data->func);
@@ -76,13 +116,26 @@ final class ExecutorGlobalsReader
             $func_pointer->cdata,
             $this->zend_type_reader->sizeOf('zend_function')
         );
-        $current_function = $this->zend_type_reader->readAs('zend_function', $current_function_raw);
+        return $this->zend_type_reader->readAs('zend_function', $current_function_raw);
+    }
 
+    /**
+     * @param int $pid
+     * @param CData $current_function
+     * @return string
+     * @throws MemoryReaderException
+     */
+    public function readFunctionName(int $pid, CData $current_function): string
+    {
         /**
-         * @var \FFI\PhpInternals\zend_function $current_function
          * @psalm-suppress PropertyTypeCoercion
+         * @psalm-var \FFI\PhpInternals\zend_function $current_function
+         * @psalm-var \FFI\CPointer $current_function_name_pointer
          */
         $current_function_name_pointer = \FFI::cast('long', $current_function->common->function_name);
+        if ($current_function_name_pointer->cdata == 0) {
+            return '<main>';
+        }
         /** @var \FFI\CPointer $current_function_name_pointer */
         $current_function_name_zstring = $this->memory_reader->read(
             $pid,
@@ -112,9 +165,8 @@ final class ExecutorGlobalsReader
             );
             /** @var \FFI\PhpInternals\zend_string $class_name_string */
             $class_name_string = $this->zend_type_reader->readAs('zend_string', $current_class_name_zstring);
-            $class_name = \FFI::string($class_name_string->val)  . '::';
+            $class_name = \FFI::string($class_name_string->val) . '::';
         }
-
         return $class_name . \FFI::string($string->val);
     }
 }
