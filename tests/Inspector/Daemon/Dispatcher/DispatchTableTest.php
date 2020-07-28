a320549f08b03441d3aef87807ce2508094910ff
sji <shinji.igarashi@gmail.com>
2020-07-29 07:50:02 +0900

    add test for DispatchTable

---
+++ tests/Inspector/Daemon/Dispatcher/DispatchTableTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file is part of the sj-i/php-profiler package.
+ *
+ * (c) sji <sji@sj-i.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace PhpProfiler\Inspector\Daemon\Dispatcher;
+
+use PhpProfiler\Inspector\Daemon\Reader\Context\PhpReaderContextInterface;
+use PhpProfiler\Inspector\Settings\GetTraceSettings;
+use PhpProfiler\Inspector\Settings\TargetPhpSettings;
+use PhpProfiler\Inspector\Settings\TraceLoopSettings;
+use PHPUnit\Framework\TestCase;
+
+class DispatchTableTest extends TestCase
+{
+    public function testUpdateTarget()
+    {
+        $attached = [];
+        $worker1 = \Mockery::mock(PhpReaderContextInterface::class);
+        $worker1->expects()
+            ->sendAttach()
+            ->withArgs(function (int $pid) use (&$attached) {
+                $attached[] = $pid;
+                return true;
+            });
+        $worker2 = clone $worker1;
+        $worker3 = clone $worker1;
+        $workers = [$worker1, $worker2, $worker3];
+        $worker_pool = \Mockery::mock(WorkerPoolInterface::class);
+        $dispatch_table = new DispatchTable(
+            $worker_pool,
+            new TargetPhpSettings(),
+            new TraceLoopSettings(1, 'test', 1),
+            new GetTraceSettings(1)
+        );
+
+        $worker_pool->expects()->getFreeWorker()->andReturns($worker1);
+        $worker_pool->expects()->getFreeWorker()->andReturns($worker2);
+        $worker_pool->expects()->getFreeWorker()->andReturns($worker3);
+        $worker_pool->expects()->getFreeWorker()->andReturns(null);
+        $dispatch_table->updateTargets(new TargetProcessList(1, 2, 3));
+        $attached_first = $attached;
+        sort($attached);
+        $this->assertSame([1, 2, 3], $attached);
+
+        $attached = [];
+        $detached = $workers[array_search(3, $attached_first, true)];
+        $worker_pool->expects()->returnWorkerToPool($detached);
+        $worker_pool->expects()->getFreeWorker()->andReturns($detached);
+        $worker_pool->expects()->returnWorkerToPool($detached);
+        $dispatch_table->updateTargets(new TargetProcessList(1, 2));
+        $this->assertSame([], $attached);
+    }
+}
