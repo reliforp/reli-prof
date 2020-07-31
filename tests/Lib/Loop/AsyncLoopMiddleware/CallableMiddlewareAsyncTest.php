4a0786238b7246e7707d6336eeb18b7bc32f2075
sji <shinji.igarashi@gmail.com>
2020-08-01 03:09:45 +0900

    add test for CallableMiddlewareAsync

---
+++ tests/Lib/Loop/AsyncLoopMiddleware/CallableMiddlewareAsyncTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of the sj-i/php-profiler package.
+ *
+ * (c) sji <sji@sj-i.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace PhpProfiler\Lib\Loop\AsyncLoopMiddleware;
+
+use PHPUnit\Framework\TestCase;
+
+class CallableMiddlewareAsyncTest extends TestCase
+{
+    public function testInvoke(): void
+    {
+        $callable_middleware = new CallableMiddlewareAsync(
+            function () {
+                yield 42;
+            }
+        );
+        $generator = $callable_middleware->invoke();
+        $result = $generator->current();
+        $this->assertSame(42, $result);
+        $result = $generator->send(null);
+        $this->assertSame(null, $result);
+    }
+}
