4d56d4c7c5094db2e2d929dfda9bfe323de74e9f
sji <sji@sj-i.dev>
2021-07-31 16:06:57 +0900

    add classes representing traces

---
+++ src/Lib/PhpProcessReader/CallTrace.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the sj-i/php-profiler package.
+ *
+ * (c) sji <sji@sj-i.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace PhpProfiler\Lib\PhpProcessReader;
+
+/** @psalm-immutable */
+final class CallTrace
+{
+    /** @var CallFrame[] */
+    public array $call_frames;
+
+    public function __construct(
+        CallFrame ...$call_frames
+    ) {
+        $this->call_frames = $call_frames;
+    }
+}
