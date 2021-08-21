70305808d7be4272f6017c2fe1885213d0ffa541
sji <sji@sj-i.dev>
2021-08-22 01:02:37 +0900

    add arithmetic and logical operations for DWARF expressions

---
+++ src/Lib/Dwarf/Expression/Opcodes/ArithmeticAndLogical/BitwiseAnd.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the sj-i/php-profiler package.
+ *
+ * (c) sji <sji@sj-i.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace PhpProfiler\Lib\Dwarf\Expression\Opcodes\ArithmeticAndLogical;
+
+use PhpProfiler\Lib\Dwarf\Expression\Opcodes\Opcode;
+use PhpProfiler\Lib\Dwarf\Expression\Stack;
+
+final class BitwiseAnd implements Opcode
+{
+    public function execute(Stack $stack, ...$operands): int
+    {
+        $stack->push($stack->pop() & $stack->pop());
+        return 1;
+    }
+}
