e5a2ae2c25820c266ea7b8fada7863ea1d01fb6e
sji <sji@sj-i.dev>
2021-08-22 01:01:18 +0900

    add some control flow operations for DWARF expressions

---
+++ src/Lib/Dwarf/Expression/Opcodes/ControlFlow/Branch.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the sj-i/php-profiler package.
+ *
+ * (c) sji <sji@sj-i.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace PhpProfiler\Lib\Dwarf\Expression\Opcodes\ControlFlow;
+
+use PhpProfiler\Lib\Dwarf\Expression\Opcodes\Opcode;
+use PhpProfiler\Lib\Dwarf\Expression\Stack;
+
+final class Branch implements Opcode
+{
+    public function execute(Stack $stack, ...$operands): int
+    {
+        if ($stack->pop() !== 0) {
+            return 3 + $operands[0];
+        }
+        return 3;
+    }
+}
