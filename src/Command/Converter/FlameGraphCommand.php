a9ad88fed29d1116064991652ba5890065f20cd2
sji <sji@sj-i.dev>
2021-10-07 01:11:33 +0900

    add a shortcut for outputting flamegraphs

---
+++ src/Command/Converter/FlameGraphCommand.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * This file is part of the sj-i/php-profiler package.
+ *
+ * (c) sji <sji@sj-i.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace PhpProfiler\Command\Converter;
+
+use Noodlehaus\Config;
+use PhpCast\Cast;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class FlameGraphCommand extends Command
+{
+    public function __construct(
+        private Config $config,
+    ) {
+        parent::__construct();
+    }
+
+    public function configure(): void
+    {
+        $this->setName('converter:flamegraph')
+            ->setDescription('convert phpspy-compatible trace file to flamegraph')
+        ;
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $tools_path = Cast::toString($this->config->get('paths.tools'));
+
+        $flamegraph = "{$tools_path}/flamegraph/flamegraph.pl";
+        $stackcollapse = "{$tools_path}/stackcollapse-phpspy/stackcollapse-phpspy.pl";
+
+        $stackcollapse_process = proc_open(
+            [
+                $stackcollapse,
+            ],
+            [
+                0 => STDIN,
+                1 => ['pipe', 'w'],
+                2 => STDERR,
+            ],
+            $stackcollapse_pipes
+        );
+        $flamegraph_process = proc_open(
+            [
+                $flamegraph,
+            ],
+            [
+                0 => $stackcollapse_pipes[1],
+                1 => STDOUT,
+                2 => STDERR,
+            ],
+            $stackcollapse_pipes
+        );
+
+        proc_close($stackcollapse_process);
+        proc_close($flamegraph_process);
+
+        return 0;
+    }
+}
